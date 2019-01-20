081ade4c386fa2a001e1271c3507c7565ef829e8
keyur2714 <keyur.555kn@gmail.com>
2019-01-20 13:10:11 +0530

    Added Basics In InbuitldEventDemo

---
+++ InBuiltEventBindingDemo/src/app/message/message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-message',
+    template : `<h1>Hello Radhe Krishna...!</h1>`,
+    styles : ['h1{color : green;}']
+})
+export class MessageComponent {
+    
+}
